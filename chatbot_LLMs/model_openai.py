5445ee0415c98161517fa02aa29a06621fb8b687
aadityaadyot <aadityaadyot@gmail.com>
2024-04-11 00:41:17 +0530

    updated

---
+++ chatbot_LLMs/model_openai.py
@@ -0,0 +1,8 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompt import ChatPromptTemplate
+from langchain_core.output_parsers import strOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
