f24a17986e38f3b40b66a6d368e2fdfd5c99bcbf
aadityaadyot <aadityaadyot@gmail.com>
2024-05-22 19:19:39 +0530

    updated

--- chatbot_LLMs/model_openai.py
+++ chatbot_LLMs/model_openai.py
@@ -1,8 +1,13 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.prompt import ChatPromptTemplate
-from langchain_core.output_parsers import strOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
 
 import streamlit as st
 import os
 from dotenv import load_dotenv
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
